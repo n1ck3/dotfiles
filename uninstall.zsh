836dac24a06335514a42af5614373bbcad510c05
Niclas Helbro <niclas.helbro@gmail.com>
2012-12-23 11:39:32 +0100

    Added a very crude uninstall script

---
+++ uninstall.zsh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+cd $HOME
+
+rm -rf etc
+rm -rf .mpd
+rm -rf .ncmpcpp
+unlink .gitconfig
+unlink .mpdconf
+unlink .terminfo
+unlink .vim
+unlink .vimrc
+unlink .tmux
+unlink .tmux.conf
+unlink .terminfo
+unlink .xsession
+unlink .zshrc
