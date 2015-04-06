47388311063b564b274fa479c827ab05ae5d14e2
Niclas Helbro <niclas.helbro@gmail.com>
2015-04-06 18:10:24 +0200

    Fixed names of keymaps

--- conf-comps/lenovo-s430/setup.sh
+++ conf-comps/lenovo-s430/setup.sh
@@ -6,7 +6,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )
 echo Run the following commands:
 
 # Console keymaps
-echo "sudo ln -s $DIR/files/se-lenovo-s430.map.gz /usr/share/kbd/keymaps/se-lenovo-s430.map.gz"
+echo "sudo ln -s $DIR/files/sv-lenovo-s430.map.gz /usr/share/kbd/keymaps/sv-lenovo-s430.map.gz"
 
 # X11 keymaps
-echo "sudo ln -s $DIR/files/se-lenovo-s430 /usr/share/X11/xkb/symbols/se-lenovo-s430"
+echo "sudo ln -s $DIR/files/sv-lenovo-s430 /usr/share/X11/xkb/symbols/sv-lenovo-s430"
