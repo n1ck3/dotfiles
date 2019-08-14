cb1046b37cc96ec7895052f2a92f0a52124d5d53
Niclas Helbro <niclas.helbro@gmail.com>
2019-08-14 14:17:23 +0200

    Tweaked local.zsh

--- zsh/custom/local.zsh
+++ zsh/custom/local.zsh
@@ -1,9 +1,16 @@
+# Make sure we have nice console colors
+source ~/.local/bin/set-console-colors.sh
+
 # Source ZSH config
 alias sc="source ~/.zshrc"
+
+# Packman and yay aliases
+function yayy() {
+    yay $@ --noconfirm
+}
 
 # Misc exports
 export EDITOR='vim'
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
-
-source ~/.local/bin/set-console-colors.sh
+export PATH=$PATH:$HOME/.local/bin
